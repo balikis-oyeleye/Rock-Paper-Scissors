9b102988231a16bf96e91f2992870c7377bd54b6
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-28 22:00:12 -1200

    Create footer.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   const [score, setScore] = useState(4);
 
   return (
-    <div className="App flex justify-center">
+    <div className="App flex justify-start flex-col items-center">
       <Header score={score} />
+      <Footer />
     </div>
   );
 }
