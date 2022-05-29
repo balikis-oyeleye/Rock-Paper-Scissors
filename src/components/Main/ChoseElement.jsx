20c162a81d96ea172d0062490f755ba3c3c509b7
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-29 00:30:21 -1200

    Modify click element function

--- src/components/Main/ChoseElement.jsx
+++ src/components/Main/ChoseElement.jsx
@@ -1,27 +1,53 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import scissors from "../../assets/images/icon-scissors.svg";
 import rock from "../../assets/images/icon-rock.svg";
 import paper from "../../assets/images/icon-paper.svg";
 
-const ChoseElement = () => {
+const ChoseElement = ({ setChoice }) => {
+  const navigate = useNavigate();
+
+  const onClick = (event) => {
+    setChoice(event.target.id);
+    console.log(event.target.id);
+    // navigate("/game");
+  };
+
   return (
     <div className="w-full flex flex-col justify-center items-center gap-y-12">
       <div className="box__1 flex justify-between items-center gap-24">
-        <div className="box__1-image__1 cursor-pointer">
-          <div className="bg-white">
-            <img src={rock} alt="rock" className="w-3/4 h-auto" />
+        <div
+          className="box__1-image__1 cursor-pointer"
+          onClick={(e) => onClick(e)}
+          id="rock"
+        >
+          <div className="bg-white" id="rock">
+            <img src={rock} alt="rock" className="w-3/4 h-auto" id="rock" />
           </div>
         </div>
-        <div className="box__1-image__2 cursor-pointer">
-          <div className="bg-white">
-            <img src={scissors} alt="scissors" className="w-3/4 h-auto" />
+        <div
+          className="box__1-image__2 cursor-pointer"
+          onClick={onClick}
+          id="scissors"
+        >
+          <div className="bg-white" id="scissors">
+            <img
+              src={scissors}
+              alt="scissors"
+              className="w-3/4 h-auto"
+              id="scissors"
+            />
           </div>
         </div>
       </div>
       <div className="box__2">
-        <div className="box__2-image__1 cursor-pointer">
-          <div className="bg-white">
-            <img src={paper} alt="paper" className="w-3/4 h-auto" />
+        <div
+          className="box__2-image__1 cursor-pointer"
+          onClick={onClick}
+          id="paper"
+        >
+          <div className="bg-white" id="paper">
+            <img src={paper} alt="paper" className="w-3/4 h-auto" id="paper" />
           </div>
         </div>
       </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ChoseElement from "./ChoseElement";
 import Game from "./Game";
 import "./main.css";
 
 const Main = () => {
+  const [choice, setChoice] = useState(null);
+  console.log(choice);
   return (
     <main className="flex flex-row justify-center items-center h-full">
       <Routes>
-        <Route path="/" element={<ChoseElement />} />
+        <Route path="/" element={<ChoseElement setChoice={setChoice} />} />
         <Route path="/game" element={<Game />} />
       </Routes>
     </main>
