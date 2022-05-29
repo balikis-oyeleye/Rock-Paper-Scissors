5e1e611358bbc5b3fae5405d91bd6c0c78818693
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-29 04:23:28 -1200

    Fix localStorage score setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -6,6 +6,18 @@ import Main from "./components/Main/Main";
 
 function App() {
   const [score, setScore] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    const scoreItem = JSON.parse(localStorage.getItem("score"));
+    if (scoreItem) {
+      setScore(scoreItem);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("score", JSON.stringify(score));
+  }, [score]);
 
   return (
     <BrowserRouter>
