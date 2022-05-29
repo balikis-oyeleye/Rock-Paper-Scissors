4da74bcbfcfbb5da885f382a9ba3da3fe892970a
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-28 23:12:15 -1200

    Update game routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
@@ -7,11 +8,13 @@ function App() {
   const [score, setScore] = useState(4);
 
   return (
-    <div className="App flex justify-start flex-col items-center">
-      <Header score={score} />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App flex justify-start flex-col items-center">
+        <Header score={score} />
+        <Main />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ChoseElement from "./ChoseElement";
+import Game from "./Game";
 import "./main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<ChoseElement />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default Main;
